df86bda407e8e79eb15f5f6acd3fe7e545b272dc
Davron <davron1104@yandex.com>
2022-06-01 11:24:31 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+// jshint esversion: 9
+
+"use strict";
+
+const Object = {
+	filename: 'immidiate',
+	localname: 'something',
+	soOn: 'someone',
+		colors: {
+			bg: 'red',
+			border: 'yellow'
+		}
+};
+
+// const {bg, border} = Object.colors;
+// console.log(bg, border);
+
+// delete Object.localname;
+// console.log(Object);
+
+
+
+
+
+
+
