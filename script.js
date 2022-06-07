4eaf7912b03a876a5115cab33e276e04e41a046a
Davron <davron1104@yandex.com>
2022-06-07 11:35:27 +0500

    stepOne

--- script.js
+++ script.js
@@ -22,4 +22,14 @@ console.log(Object.keys(something).length);
 
 
 
+// бирдан n гача сонлар йигиндисини чикарамиз
+function summary (n) { 	
+let num = 0;
+	for(let i = 1; i <= n; i++) {
+		num = num + i;		 
+	}
+	console.log(num);
+}
+
+summary(10);
 
